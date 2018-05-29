d7b5f2250820e7c7143fa72d97f834ac9b7e6ec7
Student <example@example.com>
2018-05-29 11:58:51 -0700

    Add highlights and borders

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".first").click(function() {
+    $(".first").addClass("border");
+    $(".first").addClass("highlight");
+  })
+  $(".second").click(function() {
+    $(".second").addClass("border");
+    $(".second").addClass("highlight");
+  })
+  $(".third").click(function() {
+    $(".third").addClass("border");
+    $(".third").addClass("highlight");
+  })
+});
